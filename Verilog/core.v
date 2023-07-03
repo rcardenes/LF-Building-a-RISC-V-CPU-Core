650fcb8bb5a585f293e6fb585ae0785bee1b498e
Ricardo Cárdenes <ricardo.cardenes@gmail.com>
2023-07-03 14:43:35 +0300

    SV: Basic simulation model and instruction decoding

--- Verilog/core.v
+++ Verilog/core.v
@@ -11,25 +11,74 @@ module core(clk, reset, imem_data, imem_addr, dmem_data, dmem_addr, dmem_wen);
    output [31:0]     dmem_addr;
 
    // Internal signals and FF
+   logic  [31:0]     next_pc;
    logic  [31:0]     pc;
+   /* verilator lint_off UNUSEDSIGNAL */
+   logic  [31:0]     instr;
+   logic  [6:0]      opcode;
+   logic  [2:0]      funct3;
+   // logic  [6:0]      funct7;
+   logic  [4:0]      rd;
+   logic  [4:0]      rs1;
+   logic  [4:0]      rs2;
+   logic  [31:0]     imm;
 
-   // TODO: This is to keep verilator happy about unused signals.
-   //       To be replaced with the actual design
-   reg  [31:0]     instr;
-   reg  [31:0]     data;
+   logic  [10:0]     dec_bits;
+   logic             is_r_instr;
+   logic             is_i_instr;
+   logic             is_s_instr;
+   logic             is_b_instr;
+   logic             is_u_instr;
+   logic             is_j_instr;
+   logic             imm_valid;
+   /* verilator lint_on UNUSEDSIGNAL */
+
+   assign instr[31:0]    = imem_data[31:0];
+   assign dec_bits[10:0] = {instr[30], funct3, opcode};
+
    always_ff @(posedge clk) begin
-      pc <= '0;
-      instr <= '0;
-      data <= '0;
+      pc <= 0;
+      next_pc <= 0;
+
       if (~reset) begin
-         pc <= pc + 'd4;
-         instr <= imem_data;
-         data <= dmem_data;
+         next_pc <= next_pc + 'd4;
+         pc <= next_pc;
       end
    end
 
-   assign imem_addr = pc + instr;
-   assign dmem_addr = data;
+   // Instruction decoding
+   always_comb begin
+           opcode[6:0] = instr[6:0];
+           rd[4:0]     = instr[11:7];
+           funct3[2:0] = instr[14:12];
+           rs1[4:0]    = instr[19:15];
+           rs2[4:0]    = instr[24:20];
+
+           is_r_instr  = instr[6:2] ==? 5'b011x0 ||
+                         instr[6:2] ==  5'b01011 ||
+                         instr[6:2] ==  5'b10100;
+           is_i_instr  = instr[6:2] ==? 5'b0000x ||
+                         instr[6:2] ==? 5'b001x0 ||
+                         instr[6:2] == 5'b11001;
+           is_s_instr  = instr[6:2] ==? 5'b0100x;
+           is_b_instr  = instr[6:2] ==? 5'b11000;
+           is_u_instr  = instr[6:2] ==? 5'b0x101;
+           is_j_instr  = instr[6:2] ==  5'b11011;
+
+	   imm_valid   = ~is_r_instr;
+	   // Most immediate operands need sign extended
+	   imm[31:0]   = is_i_instr ? { {21{instr[31]}}, instr[30:20] } :
+		         is_s_instr ? { {21{instr[31]}}, instr[30:25], instr[11:7] } :
+			 // Branch immediates are encoded as multiples of 2
+			 is_b_instr ? { {20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0 } :
+		         // U-Type immediates are encoded as multiples of 4096
+		         is_u_instr ? { instr[31:12], 12'b0 } :
+			 // J-Type immediates are encoded as multiples of 2
+			 is_j_instr ? { {12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0 } :
+			              32'b0;  // Default, in case of R-Type instruction
+   end
+   assign imem_addr = pc;
+   assign dmem_addr = 0;
    assign dmem_wen = 0;
 
 endmodule
--- Verilog/tb_riscv.cpp
+++ Verilog/tb_riscv.cpp
@@ -1,19 +1,98 @@
+#include <iostream>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include "Vtop.h"
 
-int main(int argc, char **argv) {
-	VerilatedContext *contextp = new VerilatedContext;
+const auto RESET_CYCLES = 2;
+const auto RUNNING_TICKS = 10;
+
+class Model {
+	int sim_time;
+	VerilatedContext *contextp;
+	Vtop* top;
+	VerilatedVcdC* tracer;
+
+public:
+	Model(int, char**);
+	virtual ~Model();
+	void tick();
+	void eval();
+	void advance();
+	void reset();
+	bool done() const;
+};
+
+Model::Model(int argc, char** argv)
+	: sim_time{0}
+{
+	contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
+	top = new Vtop{contextp};
 
-	Vtop* top = new Vtop{contextp};
+	Verilated::traceEverOn(true);
+	tracer = new VerilatedVcdC;
+	top->trace(tracer, 10);
+	tracer->open("waveform.vcd");
+	top->clock = 0;
+}
 
-	bool done = true;
-	while (!done) {
-		top->eval();
-	}
+Model::~Model() {
+	top->final();
 
+	delete tracer;
 	delete top;
 	delete contextp;
+}
+
+bool
+Model::done() const {
+	return sim_time >= RUNNING_TICKS;
+}
+
+void
+Model::tick() {
+	sim_time++;
+	top->clock = !top->clock;
+	contextp->timeInc(1);
+}
+
+void
+Model::eval() {
+	top->eval();
+	tracer->dump(contextp->time());
+}
+
+void
+Model::advance() {
+	tick();
+	eval();
+}
+
+void
+Model::reset() {
+	top->reset = 1;
+
+	if (top->clock) {
+		advance();
+	}
+
+	int cycles = 0;
+	while (cycles < RESET_CYCLES) {
+		advance();
+		if (!top->clock)
+			cycles++;
+	}
+
+	top->reset = 0;
+}
+
+int main(int argc, char **argv) {
+	Model model{argc, argv};
+
+	model.reset();
+	while (!model.done()) {
+		model.advance();
+	}
+
 	return 0;
 }
